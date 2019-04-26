7c076a36f06f3496fe574e23a85058b5750b8b04
Macy Werner <mwerner@chapman.edu>
2019-04-26 00:15:17 -0700

    Adds username.sh

--- username.sh
+++ username.sh
@@ -7,8 +7,8 @@ echo "It must start with a lower case letter"
 echo "It must contain at least three characters but no more than 12 characters"
 echo " "
 echo "Enter a username"
-read ZIP
-while echo "$ZIP" | egrep -v "^[a-z][a-z_0-9]{2,11}$" > /dev/null 2>&1
+read username
+while echo "$username" | egrep -v "^[a-z][a-z_0-9]{2,11}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username that adheres to the rules above."
 	echo "Enter a username: "
